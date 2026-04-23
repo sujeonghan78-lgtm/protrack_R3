2021ecb9a8a661fc0c4430d66d57fd0d75351ecb
sujeonghan78-lgtm <sujeonghan78@gmail.com>
2026-04-23 14:04:13 +0900

    Update models.py

--- protrack/backend/models.py
+++ protrack/backend/models.py
@@ -3,15 +3,15 @@ from typing import Optional, Any
 
 
 class ProcessUpdate(BaseModel):
-    시방출도: Optional[Any] = None
-    자재확인: Optional[Any] = None
-    자재예상: Optional[Any] = None
+    시방출도일: Optional[Any] = None
+    자재확인일: Optional[Any] = None
+    자재예상일: Optional[Any] = None
     자재입고일: Optional[Any] = None
     생산완료일: Optional[Any] = None
     검사예상일: Optional[Any] = None
-    포장완료예정: Optional[Any] = None
-    포장완료: Optional[Any] = None
-    최종납기: Optional[Any] = None
+    포장완료예정일: Optional[Any] = None
+    포장완료일: Optional[Any] = None
+    최종납기일: Optional[Any] = None
     비고: Optional[str] = None
 
     class Config:
